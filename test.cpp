7a689afce2975085459765a860f583f5a5d45c1f
Grv D <71892525+iamgd4u@users.noreply.github.com>
2021-08-31 15:34:08 +0530

    initial code

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add( int a, int b){  //complete the code to return the sum of a and b
+
+
+}
+
+int main(){
+    int a=5;
+    int b=10;
+
+    int sum = add(a,b);
+
+    cout<<sum;
+    
+}
